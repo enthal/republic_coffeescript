06cf32635a3625ca611b58a5c63e06eaa2a86fe2
Tim James <tjames@tjames.com>
2012-02-18 17:03:39 -0800

    uploader script uploads to http://s3.amazonaws.com/www.onplatosrepublic.com

--- s3curl.upload.sh
+++ s3curl.upload.sh
@@ -1,1 +1,1 @@
-(cd public/; for x in `find .`; do y=`echo $x | cut -c 3-`; echo $y; s3curl.pl --id=tim --put=$y --debug -- http://s3.amazonaws.com/timtest1/$y; done)
+(cd public/; for x in `find .`; do y=`echo $x | cut -c 3-`; echo $y; s3curl.pl --id=tim --put=$y --debug -- http://s3.amazonaws.com/www.onplatosrepublic.com/$y; done)
